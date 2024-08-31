e2b8c2ea6ed83429177830ae8545d79be6c72114
Raigester <vovaraigester@gmail.com>
2024-08-31 21:35:33 +0300

    Remove comment url from urls.py

--- userroom/urls.py
+++ userroom/urls.py
@@ -4,7 +4,6 @@ from .views import auth0_callback, login_redirect, logout
 
 
 urlpatterns = [
-    # path('',include('social_django.urls')),
     path('login/', login_redirect, name='login_redirect'),
     path('logout/', logout, name='logout'),
     path('callback/', auth0_callback, name='auth0_callback'),
