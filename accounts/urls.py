bdb6cfef3b18414a53c1d6fffafb4a5836f5bae3
Raigester <vovaraigester@gmail.com>
2024-09-26 23:57:46 +0300

    add take access key

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('auth/', include('social_django.urls', namespace='social')),
-    path('login/google/', views.google_login, name='google_login'),
+    path('login/google/', views.login, name='google_login'),
+    path('auth/complete/google-oauth2/"', views.callback, name='callback'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,33 @@
-# views.py
-
+import requests
+from django.conf import settings
 from django.shortcuts import redirect
+from django.http import JsonResponse
+from google.auth.transport import requests as google_requests
+from google.oauth2 import id_token
 
+def login(request):
+    google_auth_url = (
+        "https://accounts.google.com/o/oauth2/v2/auth"
+        "?response_type=token"
+        "&client_id={client_id}"
+        "&redirect_uri={redirect_uri}"
+        "&scope=email"
+    ).format(
+        client_id=settings.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY,
+        redirect_uri="http://localhost:8000/accounts/auth/complete/google-oauth2/"
+    )
+    return redirect(google_auth_url)
 
-def google_login(request):  # noqa: ARG001
-    return redirect('social:begin', 'google-oauth2')
+def callback(request):
+    token = request.GET.get('access_token')
+    if not token:
+        return JsonResponse({'error': 'Token is missing'}, status=400)
+
+    try:
+        # Валидация токена
+        idinfo = id_token.verify_oauth2_token(token, google_requests.Request(), settings.SOCIAL_AUTH_GOOGLE_OAUTH2_KEY)
+        email = idinfo['email']
+        # Дополнительные действия (например, создание пользователя в БД)
+        return JsonResponse({'email': email})
+    except ValueError as e:
+        return JsonResponse({'error': str(e)}, status=400)
