f4fa417b9a4c15bfeee42e236f66300cb448c447
Raigester <vovaraigester@gmail.com>
2024-09-27 20:38:45 +0300

    remake auth socket

--- userroom/services/user_service.py
+++ userroom/services/user_service.py
@@ -1,33 +1,33 @@
 import logging
+import requests
 
 from channels.db import database_sync_to_async
 
-
 logger = logging.getLogger('freenglish')
-
 
 class UserService:
     @database_sync_to_async
     def get_user_from_token(self, token):
         from accounts.models import User
-        # from accounts.utils import decode_and_verify_token
-
         try:
-            # user_data = decode_and_verify_token(token)
-            # logger.debug(f'Decoded token data: {user_data}')
+            response = requests.get(f'https://oauth2.googleapis.com/tokeninfo?access_token={token}')
 
-            # Use the 'sub' field to find the user. 'sub' give from auth0
-            # user_id = user_data.get('sub')
-            # if user_id:
-            #     # Assuming you have a method to find a user by 'sub'
-            #     user = User.objects.get(auth0_sub=user_id)  # Or another suitable method for lookup
-            #     return user
-            # else:
-            #     logger.error('User ID not found in token data')
-            #     return None
-        # except ValueError as e:
-        #     logger.error(f'Token validation error: {e}')
-            return None
+            if response.status_code == 200:
+                user_info = response.json()
+                
+                user_email = user_info.get('email')
+                if user_email:
+                    user = User.objects.get(email=user_email)
+                    return user
+                else:
+                    logger.error('Email not found in token data')
+                    return None
+            else:
+                logger.error(f'Token validation error: {response.content}')
+                return None
         except User.DoesNotExist:
             logger.error('User does not exist')
+            return None
+        except Exception as e:
+            logger.error(f'Unexpected error occurred: {e}')
             return None
