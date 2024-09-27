a70c64c6afdd5f193e412d212142ca61c51165f2
Raigester <vovaraigester@gmail.com>
2024-09-28 00:55:29 +0300

    add test join room

--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -113,3 +113,73 @@ async def test_update_room():
     assert room_exists
 
     await communicator.disconnect()
+
+@pytest.mark.django_db
+@pytest.mark.asyncio
+async def test_join_room():
+    User = get_user_model()
+    
+    # Создаем пользователя
+    user = await database_sync_to_async(User.objects.create_user)(
+        email='userforcreateroom1@example.com',
+        password='password123',
+        username='userforcreateroom1',
+        google_sub='google_unique_userforcreateroom1'
+    )
+    
+    # Создаем комнату
+    room_service = RoomService()
+    room = await room_service.create_room(
+        room_name="For Edit",
+        native_language="English",
+        language_level="Intermediate",
+        participant_limit=10,
+        creator=user
+    )
+    
+    user = await database_sync_to_async(User.objects.create_user)(
+        email='userforupdateroom@example.com',
+        password='password123',
+        username='userforupdateroom',
+        google_sub='google_unique_userforupdateroom'
+    )
+    
+    # Генерируем токен
+    token = create_test_google_token(user_id=user.id, email=user.email)
+
+    # Настраиваем соединение
+    communicator = await setup_communicator(user, token)
+
+    # Присоединяемся к комнате первый раз
+    await communicator.send_json_to({
+        'type': 'joinRoom',
+        'data': {
+            'room_id': str(room.room_id)  # Используем room_id созданной комнаты
+        }
+    })
+
+    # Проверяем ответ на первое присоединение
+    response = await communicator.receive_json_from()
+    assert response['type'] == 'success'
+    assert response['message'] == 'You have successfully joined the room "For Edit".'
+
+    # Проверяем, что пользователь добавлен в комнату
+    participants_count = await database_sync_to_async(lambda: room.current_participants.count())()
+    assert participants_count == 2  # Убедимся, что участник добавлен
+
+    # Присоединяемся к комнате второй раз
+    await communicator.send_json_to({
+        'type': 'joinRoom',
+        'data': {
+            'room_id': str(room.room_id)
+        }
+    })
+
+    # Проверяем ответ на повторное присоединение
+    response = await communicator.receive_json_from()
+    assert response['type'] == 'info'
+    assert response['message'] == 'You are already a participant in the room "For Edit".'
+
+    await communicator.disconnect()
+
+
