6fd7c7e506012de52cedd59474f9840afe4d5003
Raigester <vovaraigester@gmail.com>
2024-09-28 00:37:44 +0300

    add test for edit room

--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -53,3 +53,63 @@ async def test_create_room():
     assert room_exists
     await communicator.disconnect()
 
+
+@pytest.mark.django_db
+@pytest.mark.asyncio
+async def test_update_room():
+    User = get_user_model()
+
+    user = await database_sync_to_async(User.objects.create_user)(
+        email='testuser2@example.com',
+        password='password123',
+        username='testuser2',
+        google_sub='google_unique_user2'
+    )
+
+    token = create_test_google_token(
+        user_id=user.id,
+        email=user.email
+    )
+
+    communicator = await setup_communicator(user, token)
+
+    await communicator.send_json_to(
+        {
+            'type': 'createRoom',
+            'data': {
+                'room_name': 'Initial Room',
+                'native_language': 'English',
+                'language_level': 'Beginner',
+                'participant_limit': 5,
+            },
+        }
+    )
+
+    response = await communicator.receive_json_from()
+    assert response['type'] == 'roomCreated'
+    assert 'room' in response
+    room = response['room']
+
+    room_id = room['room_id']
+
+    await communicator.send_json_to(
+        {
+            'type': 'editRoom',
+            'data': {
+                "room_id": room_id,
+                "room_name": "Updated Room",
+                "native_language": "English",
+                "language_level": "Intermediate",
+                "participant_limit": 7
+            },
+        }
+    )
+
+    response = await communicator.receive_json_from()
+    assert response['type'] == 'success'
+    assert response['message'] == 'Room updated successfully.'
+
+    room_exists = await database_sync_to_async(lambda: UserRoom.objects.filter(room_name='Updated Room').exists())()
+    assert room_exists
+
+    await communicator.disconnect()
